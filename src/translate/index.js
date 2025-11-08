4703d55202713e2bf71b9ca24ef4684b5fe3c419
marcuswh <marcuswh@andrew.cmu.edu>
2025-11-07 23:10:17 -0500

    Fix: backend API now connected

--- src/translate/index.js
+++ src/translate/index.js
@@ -1,17 +1,17 @@
 
 /* eslint-disable strict */
-// var request = require('request');
 
 const translatorApi = module.exports;
 
-translatorApi.translate = function (postData) {
-	return ['is_english', postData];
+translatorApi.translate = async function (postData) {
+	try {
+		const TRANSLATOR_API = 'http://127.0.0.1:5000';
+		const response = await fetch(`${TRANSLATOR_API}/?content=${encodeURIComponent(postData.content)}`);
+		const data = await response.json();
+		return [data.is_english, data.translated_content || ''];
+	} catch (error) {
+		console.error('Translation API error:', error);
+		// Fallback to treating as English if API fails
+		return [true, ''];
+	}
 };
-
-// translatorApi.translate = async function (postData) {
-// 	// Edit the translator URL below
-// 	// const TRANSLATOR_API = "TODO"
-// 	// const response = await fetch(TRANSLATOR_API+'/?content='+postData.content);
-// 	// const data = await response.json();
-// 	// return ['is_english','translated_content'];
-// };
